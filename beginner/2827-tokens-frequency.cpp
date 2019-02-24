ea89dd6d7ef8e6ef4a570ee5ef61556eabfb1444
Tiago Ilieve <tiago.myhro@gmail.com>
2019-02-24 22:12:11 +0100

    2827: use 'unordered_map' instead of 'map'
    It's at least 60% faster, avoiding O(log n) lookups all the time.

--- beginner/2827-tokens-frequency.cpp
+++ beginner/2827-tokens-frequency.cpp
@@ -1,21 +1,22 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
+#include <unordered_map>
 
 using namespace std;
 
 int main() {
-    map<string,int> tokens;
-    int max;
+    unordered_map<string,int> tokens;
+    string answer;
     string input;
     string tok;
+    int max;
 
     getline(cin, input);
     transform(input.begin(), input.end(), input.begin(), ::tolower);
     max = 0;
     for (int i = 0; i < (input.length() - 1); i++) {
         tok = input.substr(i, 2);
-        if (tokens.count(tok) == 0) {
+        if (tokens[tok] == 0) {
             tokens[tok] = 1;
         } else {
             tokens[tok]++;
@@ -25,12 +26,17 @@ int main() {
         }
     }
 
+    answer = "";
     for (auto kv : tokens) {
         if (kv.second == max) {
-            cout << kv.first << ':' << kv.second << endl;
-            break;
+            if (answer == "") {
+                answer = kv.first;
+            } else if (kv.first.compare(answer) < 0) {
+                answer = kv.first;
+            }
         }
     }
+    cout << answer << ':' << max << endl;
 
     return 0;
 }
