247fd224cf561d311a65e960cc26247b9854bc00
Tiago Ilieve <tiago.myhro@gmail.com>
2019-03-24 19:53:00 +0100

    Add '1198-hashmat-the-brave-warrior.c'

---
+++ mathematics/1198-hashmat-the-brave-warrior.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    long long a, b, c;
+
+    while(scanf("%lld %lld", &a, &b) != EOF) {
+        if (a > b) {
+            c = a - b;
+        } else {
+            c = b - a;
+        }
+        printf("%lld\n", c);
+    }
+
+    return 0;
+}
